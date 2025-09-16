ef008da756a44da2c59adef31a8463841d4dbf40
Nicolas Renan Machado Dias <nicolasrenanmachado@gmail.com>
2025-09-15 21:54:05 -0300

    feat/add CteMaker to exports

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import Query from "./queryMaker.js";
 import Statement from "./statementMaker.js";
+import CteMaker from "./cteMaker";
 
 export {
   Query,
-  Statement 
+  Statement,
+  CteMaker
 }
