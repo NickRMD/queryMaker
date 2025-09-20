71c9f70a693fc5414240a3e6448d5fc3f9f7b24f
Nicolas Renan Machado Dias <nicolasrenanmachado@gmail.com>
2025-09-20 00:25:37 -0300

    docs/added documentation to types, also added support to lowercase join type

--- src/types/ColumnValue.ts
+++ src/types/ColumnValue.ts
@@ -4,6 +4,8 @@
   * It includes the column name and an optional value to be assigned to that column.
   */
 export default interface ColumnValue {
+  /** The name of the column. */
   column: string;
+  /** The value to be assigned to the column. */
   value?: any;
 }
--- src/types/SetValue.ts
+++ src/types/SetValue.ts
@@ -2,9 +2,13 @@
 /**
   * Interface representing a value to be set in a database operation.
   * It includes the column to be set, and either a direct value or a reference to another column.
+  * Only one of 'from' or 'value' should be provided.
   */
 export default interface SetValue {
+  /** The column to be set. */
   setColumn: string;
+  /** The column from which to take the value, if applicable. */
   from?: string;
+  /** The value to be assigned to the column, if applicable. */
   value?: any;
 }
