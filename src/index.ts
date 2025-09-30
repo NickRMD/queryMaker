9189b9c172548fdac2a091abb5032b2344da7e29
Nicolas Renan Machado Dias <nicolasrenanmachado@gmail.com>
2025-09-30 01:54:48 -0300

    fix/added default export to CteMaker

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import Query from "./queryMaker.js";
 import Statement from "./statementMaker.js";
 export * from './cteMaker.js'
+export { default as CteMaker } from './cteMaker.js'
 
 export {
   Query,
